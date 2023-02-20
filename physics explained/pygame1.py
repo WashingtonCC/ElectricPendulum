b40f4a2b964bb49e536840d9582186e4eb8fbb94
WashingtonCC <aa4549807@gmail.com>
2023-02-20 14:26:41 -0300

    Is it working? Nah ain't no way

--- physics explained/pygame1.py
+++ physics explained/pygame1.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-from pygameZoom import PygameZoom
+#from pygameZoom import PygameZoom
 
 
 width = 800
@@ -9,17 +9,18 @@ pygame.init()
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
-pygameZoom = PygameZoom(800, 400)
-pygameZoom.set_background((255, 255, 255))
-pygameZoom.set_zoom_strength(10000)
+#pygameZoom = PygameZoom(800, 400)
+#pygameZoom.set_background((255, 255, 255))
+#pygameZoom.set_zoom_strength(10000)
 
-Q = 2 * 10 ** (-2)
+Q = 2 * 10 ** (-1)
 k = 9 * 10 ** (-9)
 g = 9.8
 
-# 100px = .1m
-l = 0.2
-# l0 = 1
+# 1 METER = 100PX
+
+l = 2
+l0 = 1
 
 q = Q
 q0 = Q
@@ -27,29 +28,31 @@ q0 = Q
 t = 0
 dt = 0.01
 
+axis = (width/2, 0)
+
 
 class Bob:
     def __init__(self):
         self.theta = math.pi / 4
         self.thetadt = 0
 
-        self.x = l * math.sin(self.theta)
-        self.y = -l * math.cos(self.theta)
+        self.x = l * math.sin(self.theta)*100 + axis[0]
+        self.y = -l * math.cos(self.theta)*100 + axis[1]
 
         self.m = 0.5
         self.q = Q
 
     def draw(self):
         pygame.draw.lines(
-            screen, (50, 50, 50), False, ((400, 0), (self.x + 400, self.y))
+            screen, (50, 50, 50), False, (axis, (self.x, self.y))
         )
         pygame.draw.circle(
             screen,
             (100, 100, 100),
-            (self.x + 400, self.y),
+            (self.x, self.y),
             10,
         )
-        # pygame.draw.circle(screen, (100,100,100), (0, 300), 10,)
+        pygame.draw.circle(screen, (100,100,100), (axis[0], l*100 + l0*100 + axis[1]), 10,)
 
 
 def update():
@@ -66,18 +69,20 @@ while True:
         if event.type == pygame.QUIT:
             exit()
 
-    thetaddt = -g * math.sin(p.theta) / l
-    # thetaddt = -g * math.sin(p.theta) / l + k * l0 * q * q0 * math.sin(p.theta) / (
-    #    l * p.m * (l**2 - 2 * l * l0 * math.cos(p.theta) + (l + l0) ** 2) ** (3 / 2)
-    # )
+    # thetaddt = -g * math.sin(p.theta) / l
+    thetaddt = -g * math.sin(p.theta) / l + k * l0 * q * q0 * math.sin(p.theta) / (
+       l * p.m * (l**2 - 2 * l * l0 * math.cos(p.theta) + (l + l0) ** 2) ** (3 / 2)
+    )
     p.thetadt = p.thetadt + thetaddt * dt
     p.theta = p.theta + p.thetadt * dt
 
-    p.x = l * math.sin(p.theta)
-    p.y = -l * math.cos(p.theta)
+    p.x = l * math.sin(p.theta) * 100 + axis[0]
+    p.y = l * math.cos(p.theta) * 100 + axis[1]
     t = t + dt
 
-    print(p.theta)
+    #print(p.theta)
+    #print(p.x, p.y)
+    #print()
 
     update()
     screen.fill((255, 255, 255))
